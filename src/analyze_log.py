7dc2b05b57e50ce0ea05798441efccd257decd3c
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-02-17 12:11:17 -0300

    feat: req 01 - implements csv file reading and default file errors to analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,38 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import csv
 
-# starting the project
+
+def analyze_log(path_to_file):
+
+    orders = []
+    menu = set()
+    week_days = set()
+
+    # maria most ordered -> item
+    # arnaldo x hamburguers -> total
+    # joao nunca pediu -> {item, item, item}
+    # dias joao nunca foi -> {item, item}
+    try:
+        with open(path_to_file, 'r') as file:
+            reader = csv.reader(file)
+            for customer, order, day in reader:
+                orders.append({
+                    'customer': customer,
+                    'order': order,
+                    'day': day
+                })
+                menu.add(order)
+                week_days.add(day)
+    except FileNotFoundError:
+        # if file extension isn't .csv, returns:
+        # "Extensão inválida: '{nome_do_arquivo}'"
+        if not path_to_file.endswith('.csv'):
+            raise FileNotFoundError(f'Extensão inválida: {path_to_file}')
+        # if path to file isn't valid, returns an error with the message:
+        # "Arquivo inexistente: '{nome_do_arquivo}'"
+        raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
+    
+    
+
+    
+
+    print('under construction')
