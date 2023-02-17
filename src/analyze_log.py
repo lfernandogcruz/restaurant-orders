c4acc28e73a8e2f33dcb4407e1c4f8df685cc53f
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-02-17 12:24:25 -0300

    feat: req 01 - implements favorite menu item by a specific customer

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,16 +1,30 @@
 import csv
 
 
+def favourite_order(orders, customer):
+    # returns the most ordered item by a customer
+    order_counter = {}
+    favorite_item = orders[0]['order']
+
+    for order in orders:
+        if order['customer'] == customer:
+            if order['order'] in order_counter:
+                order_counter[order['order']] += 1
+            else:
+                order_counter[order['order']] = 1
+
+            if order_counter[order['order']] > order_counter[favorite_item]:
+                favorite_item = order['order']
+
+    return favorite_item
+
+
 def analyze_log(path_to_file):
 
     orders = []
     menu = set()
     week_days = set()
 
-    # maria most ordered -> item
-    # arnaldo x hamburguers -> total
-    # joao nunca pediu -> {item, item, item}
-    # dias joao nunca foi -> {item, item}
     try:
         with open(path_to_file, 'r') as file:
             reader = csv.reader(file)
@@ -30,3 +44,10 @@ def analyze_log(path_to_file):
         # if path to file isn't valid, returns an error with the message:
         # "Arquivo inexistente: '{nome_do_arquivo}'"
         raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
+    
+    # maria most ordered -> item
+    maria_favorite = favourite_order(orders, 'maria')
+    
+    # arnaldo x hamburguers -> total
+    # joao nunca pediu -> {item, item, item}
+    # dias joao nunca foi -> {item, item}
